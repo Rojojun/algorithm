74b8901e69a16e96fb7c91baeb4665de03a5aae8
Rojojun <72805486+Rojojun@users.noreply.github.com>
2022-09-14 23:25:39 +0900

    [study] 나머지가 1이되는 수
    1레벨의 알고리즘도... 응용이 충분히 된다는 사실 아시나요?

---
+++ RemainOne.java
@@ -0,0 +1,12 @@
+class RemainOne {
+    public int remainOne(int n) {
+        int answer = 0;
+        for (int i = 2; i < n; i++) {
+            if (n % i == 1) {
+                answer = i;
+                break;
+            }
+        }
+        return answer;
+    }
+}
