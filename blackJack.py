34fd9e2e7d61d700b2c7b6b21fc422770bce01f7
Hojun Na <72805486+Rojojun@users.noreply.github.com>
2024-09-23 20:57:50 +0900

    Create blackJack.py

---
+++ blackJack.py
@@ -0,0 +1,19 @@
+import time
+from itertools import count
+
+5
+start = time.time()
+
+N, M = map(int, input().split())
+array = list(map(int, input().split()))
+result = 0
+
+for i in range(N):
+    for j in range(i+1, N):
+        for k in range(j+1, N):
+            if array[i] + array[j] + array[k] > M:
+                continue
+            else:
+                result = max(result, array[i] + array[j] + array[k])
+end = time.time()
+print("time : ", end - start)
