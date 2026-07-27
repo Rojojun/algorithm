eab4cd0b719e400e3dadf0dd4e2f33aa35db4edc
rojojun <zoloman316@gmail.com>
2026-07-28 08:50:49 +0900

    투포인터로 해결

--- leetcode/69. Sqrt(x)/answer.py
+++ leetcode/69. Sqrt(x)/answer.py
@@ -1,10 +1,30 @@
-class Solution:
-    def mySqrt(self, x: int) -> int: 
-        i = 0
-        while True:
-            if i * i > x:
-                i -= 1
-                break
-            i += 1
-        return i
-        
+# class Solution:
+#    def mySqrt(self, x: int) -> int: 
+#        i = 0
+#        while True:
+#            if i * i > x:
+#                i -= 1
+#                break
+#            i += 1
+#        return i
+
+x = int(input())
+left = 0
+right = x
+mid = 0
+answer = 0
+
+while left <= right:
+    mid = (left + right) // 2
+    print(mid)
+    if (mid * mid <= x):
+        print('bigger = ', mid)
+
+        answer = mid
+        left = mid + 1
+    else:
+        print('smaller = ', mid)
+        right = mid - 1 
+
+print(answer)
+
